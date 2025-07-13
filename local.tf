859567a89c760c5192bc64200ac8eeaf9e74adbe
Kris Geyer <geyerkristoffer@gmail.com>
2025-07-13 08:10:07 +0100

    fix

--- local.tf
+++ local.tf
@@ -8,6 +8,7 @@ locals {
                     repo_name = repo_name
                     git_path = file_details.git_path
                     local_path = file_details.local_path
+                    overwrite_on_create = file_details.overwrite_on_create
                 }
             ]
         ]
